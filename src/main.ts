74dabb8bfb5ffd22c4d6c96902da282ec6efdf37
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-05-03 21:13:31 +0300

    feat: add three and scss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import './styles/main.scss';
+import { initScene } from './scenes/initScene';
+
+
 console.log('Проєкт запущено ✅');
 
 const heading = document.createElement('h1');
 heading.textContent = 'Smart Home: Старт успішний!';
 document.body.appendChild(heading);
+
+initScene();
